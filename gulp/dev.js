b0dd917f97da65046aff3e81b26d9b7dbb497e57
Daniil Sivtsov <danilklin17@gmail.com>
2023-12-27 20:37:49 +0300

    header none-adaptive

--- gulp/dev.js
+++ gulp/dev.js
@@ -104,7 +104,7 @@ gulp.task('server:dev', function () {
 });
 
 gulp.task('watch:dev', function () {
-	gulp.watch('./src/scss/**/*.scss', gulp.parallel('sass:dev'));
+	gulp.watch('./src/sass/**/*.sass', gulp.parallel('sass:dev'));
 	gulp.watch('./src/html/**/*.html', gulp.parallel('html:dev'));
 	gulp.watch('./src/img/**/*', gulp.parallel('images:dev'));
 	gulp.watch('./src/fonts/**/*', gulp.parallel('fonts:dev'));
