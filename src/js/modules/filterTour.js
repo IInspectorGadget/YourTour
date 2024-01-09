7efdd1b5c33169db10ab7d50e4fea92db92b5ea3
Daniil Sivtsov <danilklin17@gmail.com>
2024-01-09 06:09:53 +0300

    fix filter

--- src/js/modules/filterTour.js
+++ src/js/modules/filterTour.js
@@ -1,31 +1,54 @@
 export const filterTour = () => {
     const categories = document.getElementsByClassName("choose-tour__item");
     const cards = document.getElementsByClassName("choose-tour__card");
-    let enablesCategory = [];
-    let enable_count = 0;
+    let enablesCategory = JSON.parse(localStorage.getItem("filter")) || ["popular"];
+    enablesCategory = new Set(enablesCategory);
 
-    for(let category of categories ){
-        const filter = category.getAttribute("data-filter");
-        category.addEventListener('click', ()=>{
-            if (category.classList.contains("choose-tour__item-active")){
-                category.classList.remove("choose-tour__item-active");
-                enable_count-=1;
-                enablesCategory = enablesCategory.filter(e => e != filter);
-            }else{
-                enable_count+=1;
-                category.classList.add("choose-tour__item-active");
-                enablesCategory.push(filter);
-            }
-
-            for(let card of cards){
-                const type = card.getAttribute("data-type");
-                if (!enablesCategory.includes(type) && enable_count){
+    function setFilter(category, filter) {
+        //добавляем активным пунктам фильтра класс ..active
+        if (category.classList.contains("choose-tour__item-active")){
+            category.classList.remove("choose-tour__item-active");
+            enablesCategory.delete(filter);
+            localStorage.setItem("filter", JSON.stringify(Array.from(enablesCategory)));
+        }else{
+            category.classList.add("choose-tour__item-active");
+            enablesCategory.add(filter);
+            localStorage.setItem("filter", JSON.stringify(Array.from(enablesCategory)));
+        }
+        //элементам которые не соответствуют фильтру добавляем display: none
+        for(const card of cards){
+            const types = card.getAttribute("data-type");
+            const id = Number(card.getAttribute("data-id"))
+            const displayItems = []
+            for (const type of types.split(" ")){
+                if (!enablesCategory.has(type)  && displayItems.indexOf(id) == -1){
                     card.style.display = "none";
                 } else {
+                    displayItems.push(id);
                     card.style.display = "flex";
                 }
             }
             
+        }
+    }
+
+    for(const category of categories ){
+        const filter = category.getAttribute("data-filter");
+        
+        //Первичная установка фильтра из localstorage
+        if( enablesCategory.size != 0){
+            if(enablesCategory.has(filter)){
+                setFilter(category, filter);
+            }
+        }else{
+            for(const card of cards){
+                card.style.display = "none";
+            }
+        }
+
+        //Вешаем обработчик для изменения фильтра по нажатию на категорию
+        category.addEventListener('click', ()=>{
+            setFilter(category, filter);
         })
     }
 }
