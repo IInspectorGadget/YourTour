1b675e77a6043c7b901cd5d649aa84eb0e4e6a18
Daniil Sivtsov <danilklin17@gmail.com>
2024-01-12 17:51:51 +0300

    bug fix

--- src/js/modules/filterTour.js
+++ src/js/modules/filterTour.js
@@ -1,27 +1,30 @@
 export const filterTour = () => {
     const categories = document.getElementsByClassName("choose-tour__item");
     const cards = document.getElementsByClassName("choose-tour__card");
-    let enablesCategory = JSON.parse(localStorage.getItem("filter")) || ["popular"];
-    enablesCategory = new Set(enablesCategory);
+    let enablesCategory =  localStorage.getItem("filter") || "popular";
 
-    function setFilter(category, filter) {
-        //добавляем активным пунктам фильтра класс ..active
-        if (category.classList.contains("choose-tour__item-active")){
-            category.classList.remove("choose-tour__item-active");
-            enablesCategory.delete(filter);
-            localStorage.setItem("filter", JSON.stringify(Array.from(enablesCategory)));
-        }else{
-            category.classList.add("choose-tour__item-active");
-            enablesCategory.add(filter);
-            localStorage.setItem("filter", JSON.stringify(Array.from(enablesCategory)));
+    const setFilter = (curCategory, filter) => {
+
+
+        //Обновляем класс ..active
+        if (!curCategory.classList.contains("choose-tour__item-active")){
+            //Удаляем у старой категории класс актив
+            for(const category of categories){
+                category.classList.remove("choose-tour__item-active")
+            }
+            curCategory.classList.add("choose-tour__item-active");
+            enablesCategory = filter;
+            console.log(enablesCategory);
+            localStorage.setItem("filter", enablesCategory);
         }
+
         //элементам которые не соответствуют фильтру добавляем display: none
         for(const card of cards){
             const types = card.getAttribute("data-type");
             const id = Number(card.getAttribute("data-id"))
             const displayItems = []
             for (const type of types.split(" ")){
-                if (!enablesCategory.has(type)  && displayItems.indexOf(id) == -1){
+                if (enablesCategory != type  && displayItems.indexOf(id) == -1){
                     card.style.display = "none";
                 } else {
                     displayItems.push(id);
@@ -34,18 +37,10 @@ export const filterTour = () => {
 
     for(const category of categories ){
         const filter = category.getAttribute("data-filter");
-        
         //Первичная установка фильтра из localstorage
-        if( enablesCategory.size != 0){
-            if(enablesCategory.has(filter)){
-                setFilter(category, filter);
-            }
-        }else{
-            for(const card of cards){
-                card.style.display = "none";
-            }
+        if(enablesCategory == filter){
+            setFilter(category, filter);
         }
-
         //Вешаем обработчик для изменения фильтра по нажатию на категорию
         category.addEventListener('click', ()=>{
             setFilter(category, filter);
