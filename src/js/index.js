be61e95876fae93f5dd3f1e128622a81d2a6e119
Daniil Sivtsov <danilklin17@gmail.com>
2024-01-15 15:26:41 +0300

    collect-tour: replace svg icons to css styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,11 @@
 import {headerScroll} from './modules/headerScroll'
 import {filterTour} from './modules/filterTour'
+import {collectTour} from './modules/collectTour'
 
 headerScroll();
 
 filterTour();
+
+collectTour();
 
 
--- src/js/modules/headerScroll.js
+++ src/js/modules/headerScroll.js
@@ -1,29 +1,15 @@
 export const headerScroll = () =>{
-    const header = document.getElementsByClassName("header__wrapper-top")[0]
+    const header = document.getElementsByClassName("header__wrapper")[0]
     const startChangeHight = 450
 
     window.addEventListener("scroll", () =>{
         let scrollDistance = window.scrollY
 
         if (scrollDistance >= startChangeHight){
-            header.classList.add("header__wrapper-top-fixed")
+            header.classList.add("header__wrapper-scroll")
         } else {
-            header.classList.remove("header__wrapper-top-fixed")
+            header.classList.remove("header__wrapper-scroll")
         }
     })
 }
-
-// export const smoothScroll = () =>{
-//     const links = document.getElementsByClassName("header__nav-link");
-
-//     for (let link of links){
-//         link.addEventListener('click', (e) =>{
-//             e.preventDefault();
-//             document.querySelector(link.getAttribute('href')).scrollIntoView({
-//                 behavior: 'smooth'
-//             });
-//         });
-//     };
-
-// }
 
